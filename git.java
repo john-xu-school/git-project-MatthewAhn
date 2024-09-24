b35b672125b0aab4e44b0779767e45252ffd6022
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2024-09-24 12:55:34 -0700

    started Milestone #2
    added blob, sha1HashCode, and zip methods
    also started writing tester

---
+++ git.java
@@ -0,0 +1,131 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class git {
+    String pathToDirectory = System.getProperty("user.dir");
+    String gitPath = pathToDirectory + File.separator + "git";
+    String objectsPath = gitPath + File.separator + "objects";
+    String indexPath = gitPath + File.separator + "index";
+
+    int zip = 0;
+
+    // checks if files/directories exist then creates them
+    public void initializeRepository() {
+
+        File git = new File(gitPath);
+
+        if (!git.exists()) {
+            try {
+                git.mkdir();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        File objects = new File(objectsPath);
+
+        if (!objects.exists()) {
+            try {
+                objects.mkdir();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        File index = new File(indexPath);
+        if (!index.exists()) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(indexPath))) {
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if ((git.exists()) && (objects.exists()) && (index.exists())) {
+            System.out.println("Git Repo already exits");
+        }
+    }
+
+    // deletes index, objects, and git
+    public void deleteReopsitory() {
+        File git = new File(gitPath);
+        File objects = new File(objectsPath);
+        File index = new File(indexPath);
+
+        String deleted = "deleted: ";
+        if (objects.exists()) {
+            objects.delete();
+            deleted += "objects ";
+        }
+        if (index.exists()) {
+            index.delete();
+            deleted += "index ";
+        }
+        if (git.exists()) {
+            git.delete();
+            deleted += "git ";
+        }
+
+        System.out.println(deleted);
+    }
+
+    
+    public void blob(String filename){
+         // get file conents and sha1 hash
+        StringBuffer contents = new StringBuffer();
+        try {
+            FileReader f = new FileReader(filename);
+            BufferedReader file = new BufferedReader(f);
+
+            char character = (char) file.read();
+
+            while (character != (char) -1) {
+                contents.append(character);
+                character = (char) file.read();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        String contentsString = zip(contents.toString());
+
+        String hashCode = sha1HashCode(contentsString);
+        
+         // create file in objects folder with name as the hash and same contents
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(objectsPath + File.separator+ hashCode))) {
+           bufferedWriter.write(contents.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // write the hash [space] filename as a line in index
+        try{
+            BufferedWriter indexEditor = new BufferedWriter(new FileWriter(indexPath));
+            indexEditor.write(hashCode + " " + filename);
+            indexEditor.newLine();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    // generates sha1 hashcode badsed on contents
+    private String sha1HashCode(String contents) {
+        return " ";
+    }
+
+    private String zip(String contentsString){
+        String result = contentsString;
+        if (zip == 1){
+           
+        }
+        return result;
+    }
+}
--- initializeRepositoryTester.java
+++ initializeRepositoryTester.java
@@ -1,7 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class initializeRepositoryTester {
     public static void main(String[] args) {
-        initializeRepository test = new initializeRepository();
-        test.initializeRepository();
-        test.deleteReopsitory();
-    }
+    
+        git test = new git();
+    
+        // test.initializeRepository();
+        // test.deleteReopsitory();
+
+        // write data, filname and correct hash
+        String data = "hello";
+        String correctHash = "aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d";
+        String filename = "testFile";
+        // make the file and run blob on it
+        String pathToDirectory = System.getProperty("user.dir") + File.separator + "git" + File.separator;
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(pathToDirectory+filename))){
+                bufferedWriter.write(data);
+        }
+        catch(Exception e){
+            System.out.print(e);
+        }
+
+        test.blob(filename);
+        // checks if index and objects match what they should
+            // does index have "aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d testFile"
+            // does objects have "aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d" with "hello" inside it
+        }
 }
