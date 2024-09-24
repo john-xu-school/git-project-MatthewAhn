89321b2b2694ee7c6d679d666fe43f1a949f4685
MatthewRobot <147113127+MatthewRobot@users.noreply.github.com>
2024-09-24 11:33:59 -0700

    Milestone #1 finished
    made method initalizeRepository and deleteRepository
    
    also made tester

---
+++ initializeRepositoryTester.java
@@ -0,0 +1,7 @@
+public class initializeRepositoryTester {
+    public static void main(String[] args) {
+        initializeRepository test = new initializeRepository();
+        test.initializeRepository();
+        test.deleteReopsitory();
+    }
+}
